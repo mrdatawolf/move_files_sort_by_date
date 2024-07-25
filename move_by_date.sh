0b40e9d841aa65785113486364f2677471224e37
Patrick Moon <patrickmoon@outlook.com>
2024-07-25 11:42:05 -0700

    changed to a move and we now ignore current month files

--- move_by_date.sh
+++ move_by_date.sh
@@ -6,12 +6,14 @@ dest_dir="/home/biztech/test"
 # Directory to search through
 search_dir="/home/biztech/test"
 
+# Get the current month and year
+current_month_year=$(date "+%B_%Y")
+
 # Loop through all files in the search directory
 for file in "$search_dir"/*; do
     echo "checking on $file"
     # Only process files in the format "Stm_?? Daily Report *.*"
     if [[ $file =~ Stm_..[[:space:]]Daily[[:space:]]Report[[:space:]].* ]]; then
-        echo "file is properly formatted"
         # Extract the date from the end of the filename
         date_string=$(echo $file | awk -F'Report ' '{print $2}' | awk -F'.xlsx' '{print $1}')
         # Convert the date to the format "Month_Year"
@@ -19,18 +21,19 @@ for file in "$search_dir"/*; do
         IFS="-" read -r month day year <<< "$date_string"
         formatted_date_string="$year-$month-$day"
         folder_name=$(date -d$formatted_date_string "+%B_%Y")
-        echo $folder_name
+        # Check if the file is from the current month
+        if [[ "$folder_name" == "$current_month_year" ]]; then
+            # If the file is from the current month, skip it
+            continue
+        fi
         # Check if the folder exists in the dest_dir directory, if not create it
         if [[ ! -d "$dest_dir/$folder_name" ]]; then
             mkdir "$dest_dir/$folder_name"
-            echo "made folder"
-        else
-            echo "folder  exists"
         fi
         # Check if the file already exists in the target folder
         if [[ ! -f "$dest_dir/$folder_name/$(basename "$file")" ]]; then
             # If the file doesn't exist, copy it to the target folder
-            cp "$file" "$dest_dir/$folder_name/"
+            mv "$file" "$dest_dir/$folder_name/"
         fi
     else
         echo "file did not match formatting"
